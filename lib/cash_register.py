3b934fa08c633b8c275fa50bf213254dcf337bdd
Kamauleonard <leonardmwangi2012@gmail.com>
2023-12-14 14:31:43 +0300

    Completed Assignment

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 class CashRegister:
     def __init__(self, discount=0):
-        self.total = 0
-        self.last_transaction_amount = 0
         self.discount = discount
+        self.total = 0
         self.items = []
+        self.last_transaction_amount = 0
 
     def add_item(self, title, price, quantity=1):
         for _ in range(quantity):
@@ -13,22 +15,29 @@ class CashRegister:
 
     def apply_discount(self):
         if self.discount > 0:
-            discount_factor = 1 - (self.discount / 100.0)
-            self.total *= discount_factor
+            discount_amount = (self.discount / 100) * self.total
+            self.total -= discount_amount
             print(f"After the discount, the total comes to ${self.total:.2f}.")
         else:
             print("There is no discount to apply.")
 
     def void_last_transaction(self):
         if self.items:
-            last_item_price = self.last_transaction_amount
-            self.total -= last_item_price
-            self.items.pop()
-            self.last_transaction_amount = 0
+            last_item = self.items[-1]
+            last_item_quantity = self.items.count(last_item)
+            
+            self.total -= last_item_quantity * self.last_transaction_amount
+            self.items = self.items[:-last_item_quantity]
+
             if not self.items:
                 self.total = 0.0
         else:
             print("No transactions to void.")
-    
-    def get_total(self):
-        return self.total
+
+
+
+
+
+
+
+
